a2623f3c445a69a7a0d289d897cb738713354ee4
hsin <hsin919@gmail.com>
2014-09-30 20:37:02 +0800

    Add hex extension for NSData

---
+++ NSData+hex.h
@@ -0,0 +1,15 @@
+//
+//  NSData.h
+//  Encoder Demo
+//
+//  Created by Nathan on 2014/9/30.
+//  Copyright (c) 2014年 Geraint Davies. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSData(Hex)
+-(NSString*)hexRepresentationWithSpaces_AS:(BOOL)spaces;
+@end
+
+
