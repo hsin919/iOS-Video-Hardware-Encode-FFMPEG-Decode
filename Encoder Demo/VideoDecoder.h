09c169df2893c6e75c06a8e03ae5a8bac22e1d0e
hsin <hsin919@gmail.com>
2014-12-01 11:02:17 +0800

    Add VideoDecoder from Dropcam TestRtsp sample for testing FFMPEG decoding. http://rtsptest.googlecode.com/svn/trunk/TestRtsp/lib/DecoderWrapper/VideoDecoder.m

---
+++ Encoder Demo/VideoDecoder.h
@@ -0,0 +1,13 @@
+//
+//  VideoDecoder.h
+//  EncoderDemo
+//
+//  Created by Nathan on 2014/12/1.
+//  Copyright (c) 2014年 Geraint Davies. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VideoDecoder : NSObject
+
+@end
---
+++ Encoder Demo/VideoDecoder.m
@@ -0,0 +1,13 @@
+//
+//  VideoDecoder.m
+//  EncoderDemo
+//
+//  Created by Nathan on 2014/12/1.
+//  Copyright (c) 2014年 Geraint Davies. All rights reserved.
+//
+
+#import "VideoDecoder.h"
+
+@implementation VideoDecoder
+
+@end
